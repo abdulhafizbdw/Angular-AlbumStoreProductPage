04af1958309692fd5e7b2b7b0bcc409437952ac3
Abdulhafiz Bindawa <abdulhafizdw@gmail.com>
2019-11-09 00:10:48 +0100

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,11 +8,11 @@ import { ProductService } from "../product.service";
 })
 export class ProductDescriptionComponent implements OnInit {
   albumInfo;
-  constructor(private _productService: ProductService) {}
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService
-      .getAlbum(1)
-      .subscribe(response => (this.albumInfo = response)ß);
+    this._productService.getAlbum(1).subscribe(response =>
+      this.albumInfo = response);
   }
+
 }
